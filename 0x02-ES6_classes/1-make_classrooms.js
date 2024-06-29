f0ed8cdc49fcb51667c76c4fb8460af7c9df237d
Mariam <kelanimaryam1234@gmail.com>
2024-06-29 13:06:19 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	const first = new ClassRoom(19);
-	const second = new ClassRoom(20);
-	const third = new ClassRoom(34);
+  const first = new ClassRoom(19);
+  const second = new ClassRoom(20);
+  const third = new ClassRoom(34);
 
-	return [first, second, third];
+  return [first, second, third];
 }
