f6c070339386fc163fd56bb71f68ec9c373784f6
Mariam <kelanimaryam1234@gmail.com>
2024-07-05 17:34:08 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,11 @@ export default function cleanSet(set, startString) {
   let result = '';
 
   // Iterate over each value in the set
-  set.forEach(value => {
+  set.forEach((value) => {
     // Check if the value starts with startString
     if (value.startsWith(startString)) {
       // Append the substring after startString to the result with a '-'
-      result += value.slice(startString.length) + '-';
+      result += `${value.slice(startString.length)}-`;
     }
   });
 
